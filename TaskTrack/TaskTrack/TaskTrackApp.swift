806e9cd44dde13a01375420de35127c59a671f7c
Gajanan Bidwai <gajananbidwai525@gmail.com>
2025-12-29 10:34:14 +0530

    Initial Commit

---
+++ TaskTrack/TaskTrack/TaskTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTrackApp.swift
+//  TaskTrack
+//
+//  Created by Neosoft on 29/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
